66e53825aaa8b12005008c11193c9b28668d8796
sks6174 <surav_02@live.com>
2021-06-10 09:27:22 +0300

    handle exceptions

--- p02.c
+++ p02.c
@@ -8,36 +8,50 @@
 
 int main()
 {
-    float a, b, c, root1, root2;
+    float a, b, c, root; // root variable is for equations having single root
 
     printf("Quadratic equation: ax^2 + bx + c = 0\n");
     printf("Enter the values of a, b, and c: ");
     scanf("%f %f %f", &a, &b, &c);
 
-    float discriminant = (b * b) - 4 * a * c;
-
-    if (discriminant > 0)
+    if (a == 0 && b == 0 && c == 0)
+        printf("The equation has infinte solutions.");
+    else if (a == 0 && b == 0)
+        printf("The equation has no solution.");
+    else if (a == 0)
     {
-        root1 = (-b + sqrt(discriminant)) / (2 * a);
-        root2 = (-b - sqrt(discriminant)) / (2 * a);
-
-        printf("The quadratic equation has real roots.\n");
-        printf("Root1 = %.2f\n", root1);
-        printf("Root2 = %.2f", root2);
-    }
-    else if (discriminant < 0)
-    {
-        float first_part = -b / (2 * a);
-        float second_part = sqrt(abs(discriminant)) / (2 * a);
-
-        printf("The quadratic equation has imaginary roots.\n");
-        printf("Root1 = %.2f + %.2f i\n", first_part, second_part);
-        printf("Root2 = %.2f - %.2f i", first_part, second_part);
+        printf("The equation is not quadratic.\n");
+        root = -c / b;
+        printf("Root = %.2f", (root == -0.0f) ? 0 : root);
     }
     else
     {
-        printf("The quadratic equation has equal roots.\n");
-        printf("Root = %.2f", -b / (2 * a));
+        float discriminant = (b * b) - 4 * a * c;
+
+        if (discriminant > 0)
+        {
+            float root1 = (-b + sqrt(discriminant)) / (2 * a);
+            float root2 = (-b - sqrt(discriminant)) / (2 * a);
+
+            printf("The quadratic equation has real roots.\n");
+            printf("Root1 = %.2f\n", root1);
+            printf("Root2 = %.2f", root2);
+        }
+        else if (discriminant < 0)
+        {
+            float first_part = -b / (2 * a);
+            float second_part = sqrt(abs(discriminant)) / (2 * a);
+
+            printf("The quadratic equation has imaginary roots.\n");
+            printf("Root1 = %.2f + %.2f i\n", first_part, second_part);
+            printf("Root2 = %.2f - %.2f i", first_part, second_part);
+        }
+        else
+        {
+            printf("The quadratic equation has equal roots.\n");
+            root = -b / (2 * a);
+            printf("Root = %.2f", root);
+        }
     }
 
     getch();
