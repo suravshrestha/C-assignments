858a4b5d0766604f74972069bd202a014c6f6414
sks6174 <surav_02@live.com>
2021-06-20 12:01:39 +0545

    add practice programs

---
+++ Practice/p09.c
@@ -0,0 +1,34 @@
+/*
+    9 Write a program to read an integer and check if it is palindrome or not.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    unsigned int num, num_copy, digit;
+    int reverse = 0;
+
+    printf("C program to check whether an integer is Palindrome or not\n");
+
+    printf("Enter a positive integer: ");
+    scanf("%u", &num);
+
+    num_copy = num;
+
+    while (num_copy)
+    {
+        digit = num_copy % 10;
+        reverse = reverse * 10 + digit;
+        num_copy /= 10;
+    }
+
+    if (reverse == num)
+        printf("%u is a Palindrome number.", num);
+    else
+        printf("%u is not a Palindrome number.", num);
+
+    getch();
+    return 0;
+}
---
+++ Practice/p10.c
@@ -0,0 +1,37 @@
+/*
+    10. Write a program to read an integer and check if it is Armstrong or not.
+*/
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h> // log10() pow()
+
+int main()
+{
+    unsigned int num, num_copy, n_digits, digit;
+    int sum = 0;
+
+    printf("C program to check whether an integer is Armstrong or not\n");
+
+    printf("Enter a positive integer: ");
+    scanf("%u", &num);
+
+    n_digits = log10(num) + 1;
+
+    num_copy = num;
+
+    while (num_copy)
+    {
+        digit = num_copy % 10;
+        sum += pow(digit, n_digits);
+        num_copy /= 10;
+    }
+
+    if (sum == num)
+        printf("%u is an Armstrong number.", num);
+    else
+        printf("%u is not an Armstrong number.", num);
+
+    getch();
+    return 0;
+}
