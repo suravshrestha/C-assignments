9cea537fefebaa6e65b27a1bdd221bb1f2fb684d
sks6174 <surav_02@live.com>
2021-07-06 16:40:12 +0545

    use putch() and putchar()

--- Lab-3-console-input-output/p01.c
+++ Lab-3-console-input-output/p01.c
@@ -1,6 +1,6 @@
 /*
     Lab 3
-    1.  Write a program to read a character using getche() and getchar() and display using putch() or putche()
+    1.  Write a program to read a character using getche() and getchar() and display using putch() or putchar()
 */
 
 #include <stdio.h>
@@ -10,7 +10,7 @@ int main()
 {
     char ch;
 
-    printf("C program to show console character input\n");
+    printf("C program to show console character input and output\n");
 
     /*
         getchar() is defined in stdio.h header file
@@ -18,16 +18,25 @@ int main()
         both getche() and getchar() read a character 
         getche() doesn't wait for an enter key, while getchar() expects an enter key
         if multiple characters are entered, getchar() will read the first character only.
+
+        putchar() is standard C function (defined in stdio.h header file) while putch() is not
+        putch() is defined in the conio.h header file.
     */
     printf("With getchar():\n");
     printf("Enter a character: ");
     ch = getchar();
-    printf("The entered character is: %c", ch);
+
+    printf("With putch():\n");
+    printf("The entered character is: ");
+    putch(ch);
 
     printf("\n\nWith getche():");
     printf("\nEnter another character: ");
     ch = getche();
-    printf("\nThe entered character is: %c", ch);
+
+    printf("\nWith putchar():");
+    printf("\nThe entered character is: ");
+    putchar(ch);
 
     // getch() is similar to getche(), they don't wait for an enter key
     getch();
