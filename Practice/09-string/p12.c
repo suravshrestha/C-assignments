0b19cf9c6de467d02ec09df548b2ad1d2a63b94c
sks6174 <surav_02@live.com>
2021-07-27 07:35:34 +0545

    rename files

--- Practice/09-string/p12.c
+++ Practice/09-string/p12.c
@@ -1,14 +1,29 @@
 /*
     Practice
     String
-    12. Write a program to read a string and reverse the string using string handling function.
+    11. Write a program to read a string and reverse the string without using the string handling function strrev.
+        Create a separate user defined function, duplicating the strrev function, without the return string functionality.
 */
 
 #include <stdio.h>
 #include <conio.h>
-#include <string.h> // for strrev()
+#include <string.h> // for strlen()
 
 #define STRING_LENGTH 1000
+
+void reverse_string(char string[])
+{
+    int len = strlen(string);
+
+    for (int i = 0; i < len / 2; i++)
+    {
+        // swap characters string[i] and string[len - i - 1]
+        char temp;
+        temp = string[i];
+        string[i] = string[len - i - 1];
+        string[len - i - 1] = temp;
+    }
+}
 
 int main()
 {
@@ -21,8 +36,9 @@ int main()
 
     printf("\nEntered string: %s", string);
 
-    // strrev returns the reverse string
-    printf("\nReversed string: %s", strrev(string));
+    reverse_string(string);
+
+    printf("\nReversed string: %s", string);
 
     getch();
     return 0;
